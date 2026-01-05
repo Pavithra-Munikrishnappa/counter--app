18e27951bcc46e56d9fbab8a41ad91f37421d76a
Pavithra Munikrishnappa <107239244+Pavithra-Munikrishnappa@users.noreply.github.com>
2026-01-05 21:11:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello!');
+let count = 0;
+
+const countElement = document.getElementById('count');
+
+function increment() {
+  count++;
+  updateCount();
+}
+
+function decrement() {
+  if (count > 0) {
+    count--;
+  }
+
+  updateCount();
+}
+
+function reset() {
+  count = 0;
+  updateCount();
+}
+
+function updateCount() {
+  countElement.textContent = count;
+}
